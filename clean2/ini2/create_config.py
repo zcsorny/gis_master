a3843e3d24741b39f7f0c0658b59c08cf73f7b89
zcsorny <zcsorny@gmail.com>
2022-05-21 01:04:49 -0400

    cleaned up parts of file structure/architecture. added so notes

---
+++ clean2/ini2/create_config.py
@@ -0,0 +1,62 @@
+## structure 
+##  each GIS county will have its own pipeline (folder)
+##  each pipeline has one of many sub-folders 
+##  subfolders represent specifc/freq./intentded in the data eng. process
+## e.g. :   ### FL__LEE
+
+''' Creates .INI config file containing all config variables related to pulling & processing GIS data '''
+from datetime import datetime
+import configparser
+
+def create_ini():
+    ''' defines section/option values and creates ini file for gis data '''
+
+    config = configparser.ConfigParser()
+    
+    config['database'] = {
+        "database.dbms":        "mysql",
+        "database.db":          "gis_test",
+        "database.username":	"flask",
+        "database.password":    "Lifetime!098",
+        "database.host":        "127.0.0.1",
+        "database.port":        "3306",
+        }
+
+
+## ====================================================
+#   start of GIS county configurations 
+## ====================================================
+
+    config['fl_charlotte'] = {
+            'state':            'fl',
+            'county':           "charlotte",
+            'trial':            "no",
+            'folder_path':      "/home/lpusa-admin/datpipe/external/GIS/%(state)s/%(county)s",
+            'url':          "https://www.ccappraiser.com/downloads/charlotte.zip",
+            'source_filename':  "cd.txt",
+            'notes':            "Property appraisers table updates nightly",
+            #'raw_dpath':    "${pipelines:raw}s/${state}s/${county}/"
+    }
+
+    config['fl_lee'] = {
+            'state':            'fl',
+            'county':           "lee",
+            'trial':            "no",
+            'folder_path':      "/home/lpusa-admin/datpipe/external/GIS/%(state)s/%(county)s",
+            'url':          "https://www.arcgis.com/sharing/rest/content/items/d6c42c6e3268484a8512b7ec57ecdb2f/data",
+            'source_filename':  "Parcels.shp",
+#            'notes':                ""
+    }
+
+#    config['download_types'] = {
+#            'raw',
+#            'staging',
+#            'consumption'
+#            }
+
+    ## creating ini file with above config params
+    with open('gis_data.ini', 'w') as f:
+        config.write(f)
+
+if __name__ == "__main__":
+    create_ini()
---
+++ clean2/ini2/download_GIS_2.py
@@ -0,0 +1,102 @@
+import configparser
+import os 
+import pymysql 
+import requests
+import re
+from datetime import datetime
+from pathlib import Path
+from pprint import pprint
+from zipfile import ZipFile 
+
+# import argparse
+#import fiona
+#import pandas as pd
+
+
+## different paths for each countny 
+##      raw -- zip file of raw download data
+##      staging -- path "stages" (cleans) csv 
+##      consumption -- path uploads to db 
+
+## defining config parser       **might be able to but in own function
+inipath = 'gis_data.ini' 
+gisconfig = configparser.ConfigParser()
+gisconfig.read(inipath)
+print(f"{gisconfig.sections()}")
+
+def get_download_path(gis_job,download_type):
+    ''' download_type is either raw zip (original), staging, consumption'''
+
+    # datetime variables that will be configured upong function invocation
+    datestamp = datetime.today().strftime('%Y%m%d')
+    print(f'{datestamp}')
+    # TODO validation check for download_type input
+    valid_download_types = ['raw','statging','consumption']
+    basepath = gisconfig[gis_job]['folder_path']
+    
+    if download_type == 'raw':
+        # spreading f-string out over multiple lines for readability - will be single line string when invokedc:w
+        path = (
+                    f"{basepath}/{download_type}/"
+                    f"{datestamp}.{gisconfig.get(gis_job,'state').upper()}"
+                    f"-{gisconfig.get(gis_job,'county').upper()}.zip"
+                    )
+        print(f"{path     = }\n")
+        return Path(path)
+## TODO STATUS SUMMARY: nearly done with config file ;; starting to get baseline mappings/configs load instananeously && easily
+## TODO ADD NEW SARASOTA COUNTY for MICHAELA once first two are sorted out
+
+    elif download_type == "staging":
+        path = f"{basepath}/{download_type})"
+        print(f"{path     = }")
+        return f"{basepath}/{staging}"
+
+    elif  download_type == "consumption":
+        path = f"{basepath}/{download_type}"
+        print(f"{path     = }")
+        return f"{basepath}/{download_type}"
+
+    else:
+        ## lg.info. can be 
+        print("NEED TO CHANGE THOS TP :PG STMNT")
+
+
+def download_zip(gis_job,download_type):
+    ''' downloads GIS DATA from specific site to precofnigred pass '''
+
+    ## calling helper function to get base folder path for county
+    download_path = get_download_path('fl_charlotte','raw')
+    #download_path = get_download_path('fl_charlotte','do')
+ 
+    ### TODO = WRAP UP TESTING FOR THIS FUNCT
+        # make dirctory in case doesnt exist 
+        # print still testing       **TODO
+    print("\n\n Checking if basepath dir exist\n")
+    basepath, _ = os.path.split(download_path)
+    print(f"\n{basepath = }")
+    os.makedirs(basepath,exist_ok=True)          ## THIS NEEDS TO BE USED/COVERS ANY data job and not JUST Luka exclusevily
+    # abspath = Path(f'{download_path}/{<<RAW>>}/{<<SAVED>>}')      **** ONLY RURUNNING Oa
+
+    # downloading zip 
+    # TEMPORARILY USING (just to confirm deserts are done)['folder_path']
+    url = gisconfig[gis_job]['url']
+    print(f"\n\nSPUDS:   {url}\n\n")
+    r = requests.get(url,stream=True)
+
+    with open(Path(download_path),'wb') as dwnld:
+        for chunk in r.iter_content(chunk_size=128):
+            dwnld.write(chunk)
+    try:
+        with open(download_path,'wb') as dwld_data:
+            for chunk in r.iter_content(chunk_size=128):
+                # TODO add downloadding print statements for job status/ completion awareness 
+                dwld_data.write(chunk)
+                
+    except:
+        print("Issue with downloading data ...")
+
+
+download_zip('fl_charlotte','raw')
+
+
+    
